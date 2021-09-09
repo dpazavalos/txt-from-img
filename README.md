78c75043326b3f781479fe5cc8e691094f7d543f
Daniel Avalos <dpazavalos@gmail.com>
2021-09-08 21:46:41 -0500

    Works. Not expanding at this time

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # txt-from-img
 https://towardsdatascience.com/read-text-from-image-with-one-line-of-python-code-c22ede074cac
+
+Simple img reader
+
+pip install opencv-python
+sudo apt update
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+
+python txt_from_img.py [image]
+
--- txt_from_img.py
+++ txt_from_img.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 import cv2
 import pytesseract
 
@@ -5,7 +7,11 @@ tess_file = '/usr/bin/tesseract'
 
 pytesseract.pytesseract.tesseract_cmd = tess_file
 
-img = cv2.imread('breaking.png')
+
+# img = cv2.imread('breaking.png')
+img = cv2.imread(argv[1])
 
 text = pytesseract.image_to_string(img)
 print(text)
+
+
