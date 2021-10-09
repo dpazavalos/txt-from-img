b1796230a11c384fa1aac7575f253eb1349738a7
dpazavalos <dpazavalos@gmail.com>
2021-10-09 12:58:02 -0500

    windows file location. scraped txt to clipboard

--- txt_from_img.py
+++ txt_from_img.py
@@ -2,8 +2,10 @@ from sys import argv
 
 import cv2
 import pytesseract
+import pyperclip
 
-tess_file = '/usr/bin/tesseract'
+# tess_file = '/usr/bin/tesseract'
+tess_file = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 pytesseract.pytesseract.tesseract_cmd = tess_file
 
@@ -12,6 +14,6 @@ pytesseract.pytesseract.tesseract_cmd = tess_file
 img = cv2.imread(argv[1])
 
 text = pytesseract.image_to_string(img)
-print(text)
-
+# print(text)
+pyperclip.copy(text)
 
